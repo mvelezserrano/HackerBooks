5a314091eb02ba93e2d27854ed56d551be5f0baf
Miguel Ángel Vélez <mvelezserrano@outlook.es>
2015-03-26 19:22:29 +0100

    Initial Commit

---
+++ HackerBooks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackerBooks
+//
+//  Created by Mixi on 26/3/15.
+//  Copyright (c) 2015 Mixi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
